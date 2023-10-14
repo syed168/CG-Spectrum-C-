b1aebbc9004db3e1f4183a3ca4497c81e488d85a
kabeer\syed <ACEonepeace168@gmail.com>
2023-10-14 21:20:12 +0530

    examples

---
+++ Example.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello World" << std::endl;
+    std::cout << "My name is kabeer" << std::endl;
+
+    std::cout << "this is a test" << std::endl;
+}
